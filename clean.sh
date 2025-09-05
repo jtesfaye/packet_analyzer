4917f2a2c1cf8b3f0867ab6404f4b89e67ef3788
jtesfaye <jeremiahte@gmail.com>
2025-09-05 11:40:41 -0500

    update

--- clean.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-find . -name 'CMakeFiles' -type d -exec rm -rf {} +
-find . -name 'CMakeCache.txt' -delete
-find . -name 'cmake_install.cmake' -delete
-find . -name 'Makefile' -delete
-rm -rf build/ cmake-build-debug/
