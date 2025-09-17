ee3a53b35dd4090fb8631ffeddb0b4512963b27a
jtesfaye <jeremiahte@gmail.com>
2025-09-17 16:21:10 -0500

    changes

--- src/layerx/layer3/IPv4.cpp
+++ src/layerx/layer3/IPv4.cpp
@@ -66,7 +66,6 @@ std::unique_ptr<NetworkPDU> IPv4_functions::ipv4_parse(
     const auto ipv4_hdr = reinterpret_cast<const ipv4_header*> (raw_data.data() + start);
 
     //If not equal probably means we have malformed data or misaligned data
-
     if (u_int8_t version = (ipv4_hdr->version_ihl >> 4) & 0x0F; version != 4) {
         std::cout << version << std::endl;
         std::cout << "misaligned\n";
@@ -74,13 +73,13 @@ std::unique_ptr<NetworkPDU> IPv4_functions::ipv4_parse(
     }
 
     bool is_fragmented = false;
-    u_int16_t frag_field = ntohs(ipv4_hdr->flags_foffset);
-    u_int8_t flags = (frag_field >> 13) & 0x07;
-    u_int16_t offset = frag_field & 0x1FFF;
+
+    const u_int16_t frag_field = ntohs(ipv4_hdr->flags_foffset);
+    const u_int8_t flags = (frag_field >> 13) & 0x07;
+
     //more fragment flag is set
-    if ((flags & 0x01) || (offset > 0)) {
+    if (u_int16_t offset = frag_field & 0x1FFF; (flags & 0x01) || (offset > 0)) {
         is_fragmented = true;
-        //fragmentation logic
     }
 
     size_t header_len = (ipv4_hdr->version_ihl >> 4) * 5;
@@ -95,6 +94,5 @@ std::unique_ptr<NetworkPDU> IPv4_functions::ipv4_parse(
         PacketRead::format_ipv4_src_dst(ipv4_hdr->dest_adr),
         is_fragmented,
         next_protocol);
-
 }
 
