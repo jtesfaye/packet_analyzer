640c886a51b557d96eab89e370ff0e00a598dbcf
jtesfaye <jeremiahte@gmail.com>
2025-10-02 14:52:28 -0500

    add ipv6 parsing

--- src/layerx/layer3/IPv6.cpp
+++ src/layerx/layer3/IPv6.cpp
@@ -5,6 +5,7 @@
 #include <layerx/layer3/IPv6.h>
 #include <layerx/iana_numbers.h>
 #include <utility>
+#include <util/PacketRead.h>
 
 IPv6::IPv6(size_t len, std::string src, std::string dest, u_int8_t protocol)
 : NetworkPDU(len, std::move(src), std::move(dest))
@@ -50,10 +51,32 @@ Layer3Registry &IPv6_functions::get_ipv6_registry() {
 }
 
 std::unique_ptr<NetworkPDU> IPv6_functions::ipv6_parse(
-    const std::vector<std::byte>&,
-    packet::parse_context&) {
+    const std::vector<std::byte>& raw_data,
+    packet::parse_context& context) {
+    size_t start = context.offset;
 
-    return {};
+    if (!PacketRead::valid_length(raw_data, start, sizeof(ipv6_header))) {
+        return nullptr;
+    }
+
+    const auto ipv6_hdr = reinterpret_cast<const ipv6_header*> (raw_data.data() + start);
+
+    if (u_int8_t version = ipv6_version(ipv6_hdr); version != 6) {
+        return nullptr;
+    }
+
+    size_t payload_length = ntohs(ipv6_hdr->payload_length);
+    size_t next_protocol = ipv6_hdr->next_header;
+
+    context.curr_length = payload_length;
+    context.next_type = next_protocol;
+
+    return std::make_unique<IPv6>(
+        payload_length,
+        PacketRead::format_ipv6_src_dest(ipv6_hdr->src_addr),
+        PacketRead::format_ipv6_src_dest(ipv6_hdr->dst_addr),
+        next_protocol
+    );
 }
 
 
