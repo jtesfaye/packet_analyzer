98db23c13f55abe7f8822bf513dc24f084557e52
jtesfaye <jeremiahte@gmail.com>
2025-11-24 14:52:40 -0600

    add StartupWindowController

--- include/controller/StartupWindowController.h
+++ include/controller/StartupWindowController.h
@@ -4,5 +4,28 @@
 
 #ifndef STARTUPWINDOWCONTROLLER_H
 #define STARTUPWINDOWCONTROLLER_H
+#include <QObject>
+#include <session/CaptureSession.h>
+#include <view/StartupWindow.h>
+#include <controller/CaptureController.h>
+
+class StartupWindowController : public QObject {
+    Q_OBJECT
+
+public:
+
+    StartupWindowController();
+
+private:
+
+    void init_start_btn(QPushButton* btn);
+    void init_stop_btn(QPushButton* btn);
+    void connect_session_form_to_main();
+
+    StartupWindow* main_window;
+    CaptureController* capture_controller;
+    std::weak_ptr<CaptureSession> current_session;
+
+};
 
 #endif //STARTUPWINDOWCONTROLLER_H
